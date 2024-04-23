9b371bbbe39c3764f5c4603b1b7716e3ade0e19e
Benedicte Emly Solberg Johansen <benedicte.johansen@protonmail.com>
2024-04-23 21:41:57 +0200

    Remove comments and unused importetd packages
    Remove comments and imported packages that are not used

--- src/main/java/com/example/oblig3data1700/TicketController.java
+++ src/main/java/com/example/oblig3data1700/TicketController.java
@@ -1,17 +1,12 @@
 package com.example.oblig3data1700;
 
-import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOError;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 @RestController
